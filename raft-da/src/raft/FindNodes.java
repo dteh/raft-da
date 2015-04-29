a284dc432b46a34121cad34be8bd0b3803f1efc5
dteh <d.we.teh@gmail.com>
2015-04-29 12:24:42 +1000

    Fixed potential empty IP list bug

--- raft-da/src/raft/FindNodes.java
+++ raft-da/src/raft/FindNodes.java
@@ -1,12 +1,23 @@
 package raft;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Collections;
 import org.jgroups.*;
 import org.jgroups.stack.IpAddress;
 
+/**
+ * 
+ * @author danielteh
+ * FindNodes()
+ * Uses broadcast to find the addresses of all other nodes who
+ * have joined the 'channel'. Saves list of IPs to a list. Rescans
+ * for new addresses once every second
+ *
+ * 
+ */
 public class FindNodes {
 	JChannel channel;
+	List<String> tmpadd = new ArrayList<String>();
 	List<String> addresses = new ArrayList<String>();
 
 	
@@ -16,26 +27,27 @@ public class FindNodes {
 		getIPs();
 	}
 	
-	public void kill() throws Exception{
+	public void kill(){
 		channel.close();
 	}
 	
 	private void getIPs() throws Exception{
 		while(true){
+			tmpadd.clear();
 			Thread.sleep(1000);
-
 			System.out.println(channel.getView().getMembers());
 			List<org.jgroups.Address> members = channel.getView().getMembers();
 			for (org.jgroups.Address member: members){
 				PhysicalAddress physicalAddr = (PhysicalAddress)channel.down(new Event(Event.GET_PHYSICAL_ADDRESS,member));
 				IpAddress ipAddr = (IpAddress)physicalAddr;
 				System.out.println(ipAddr.getIpAddress().getHostAddress());
-				addresses.add(ipAddr.getIpAddress().getHostAddress());
+				tmpadd.add(ipAddr.getIpAddress().getHostAddress());
 			}
+			java.util.Collections.copy(addresses, tmpadd);
 		}
 	}
 	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		new FindNodes().start();
 
 	}
